4bc5fba49f424e9e46606d56f3f79d98405f0fe1
Jan Hendrik Mangold <jhm@cotren.net>
2015-02-10 13:48:33 -0800

    added git url

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Easy interface to create a bootstrap-3 panel-group',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/jamgold/jamgold-bootstrap3panels.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
